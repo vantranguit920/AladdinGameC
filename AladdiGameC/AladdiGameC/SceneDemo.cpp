9b82328bc3460d242f3310bd668e54c396636bdd
StoneThang <stone.datan97@gmail.com>
2019-11-19 10:47:16 +0700

    revert 2

--- AladdiGameC/AladdiGameC/SceneDemo.cpp
+++ AladdiGameC/AladdiGameC/SceneDemo.cpp
@@ -1,6 +1,5 @@
 #include "SceneDemo.h"
 
-
 SceneDemo::SceneDemo() {
 }
 SceneDemo::~SceneDemo() {
@@ -25,78 +24,18 @@ void SceneDemo::SaveGridToFile(int level) {
 	std::ofstream file;
 	std::string filename = "";
 	file.open(filename);
-	
+	//for (auto o : GRID->objects)
+	//{
+	//	// get object info
+	//	switch(o->tag)
+	//	{
 
-	grid->GetInstance();
-	for (auto o : grid->objects)
-	{
-		// get object info
-		switch(o->tag)
-		{
-		case 0:
-			// just demo
-			o->GetPosition;
-			o->GetWidth();
-			o->GetHeight();
-			break;
-		case 1:
-			break;
-
-		case 2:
-			break;
-
-		case 3:
-			break;
-
-		case 4:
-			break;
-
-		case 5:
-			break;
-
-		case 6:
-
-			break;
-		default:
-			break;
-		}
-	}
+	//	}
+	//}
 
 	file.close();
 }
 
-void SceneDemo::ReloadResources(int level) {
-	std::vector<RECT> grounds;
-	if (level == 1)
-		return;
-	switch (level) {
-	case 2:
-		break;
-	case 3:
-		break;
-	default:
-		break;
-	}
-}
-void SceneDemo::LoadGridFromFile(int level) {
-	std::ifstream file;
-	std::string filename = "";
-	file.open(filename);
-	grid->ResetGrid();
-	if (!file.good()) {
-		file.close();
-
-		// add ground from file to cell
-		 
-	}
-	
-	while (!file.eof()) {
-		std::string objectname;
-		file >> objectname;
-	}
-	file.close();
-
-}
 void SceneDemo::ReleaseAll(){
 
 }
--- AladdiGameC/AladdiGameC/SceneDemo.h
+++ AladdiGameC/AladdiGameC/SceneDemo.h
@@ -17,8 +17,7 @@ class SceneDemo :public Scene
 private:
 	Map* map;
 	Object* object;
-	Grid* grid = new Grid();
-
+	
 	std::unordered_set<Object*> visibleObject;
 	std::vector<Object*> listObject;
 	int currentLevel;
